8722e05ac157d17ea0a584f3185db02f414b693c
linglong233 <1911603279@qq.com>
2022-01-28 16:24:33 +0800

    change some item`s error name

--- src/main/java/com/pinball3d/zone/item/ItemLoader.java
+++ src/main/java/com/pinball3d/zone/item/ItemLoader.java
@@ -29,7 +29,7 @@ public class ItemLoader {
 
 	//233
 	public static Item basic_circuit_board, iron_impeller, motor, cable_cutters, iron_rivet, iron_rod, steel_plate,
-			steel_ingot, iron_grain, garton_metal_plate, garton_metal_ingot, steel_grain, steel_rivet, garton_metal_grain,
+			steel_ingot, garton_metal_plate, garton_metal_ingot, steel_nugget, steel_rivet, garton_metal_nugget,
 			garton_metal_rivet, steel_rod, garton_metal_rod, gold_wire;
 
 	public static Item energy, construct_block, fc, manual, crucible, casting_table, hybrid_fuel, pipette;
@@ -180,12 +180,12 @@ public class ItemLoader {
 		register(registry, iron_rod = new ZoneItem("iron_rod"));
 		register(registry, steel_ingot = new ZoneItem("steel_ingot"));
 		register(registry, steel_plate = new ZoneItem("steel_plate"));
-		register(registry, iron_grain = new ZoneItem("iron_grain"));
+//		register(registry, iron_nugget = new ZoneItem("iron_nugget"));
 		register(registry, garton_metal_plate = new ZoneItem("garton_metal_plate"));
 		register(registry, garton_metal_ingot = new ZoneItem("garton_metal_ingot"));
-		register(registry, steel_grain = new ZoneItem("steel_grain"));
+		register(registry, steel_nugget = new ZoneItem("steel_nugget"));
 		register(registry, steel_rivet = new ZoneItem("steel_rivet"));
-		register(registry, garton_metal_grain = new ZoneItem("garton_metal_grain"));
+		register(registry, garton_metal_nugget = new ZoneItem("garton_metal_nugget"));
 		register(registry, garton_metal_rivet = new ZoneItem("garton_metal_rivet"));
 		register(registry, steel_rod = new ZoneItem("steel_rod"));
 		register(registry, garton_metal_rod = new ZoneItem("garton_metal_rod"));
